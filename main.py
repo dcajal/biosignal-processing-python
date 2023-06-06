3bda2067b43b0f5e8ef79979e3032a458330aefc
Diego Cajal <dcajal95@gmail.com>
2023-06-06 13:19:27 +0200

    Pulse detection and time-domain HRV metrics

---
+++ main.py
@@ -0,0 +1,32 @@
+import numpy as np
+from scipy.interpolate import CubicSpline
+
+from lib.delineation import ppg_pulse_detection
+from lib.filters import filtering_and_normalization, remove_impulse_artifacts
+from lib.hrv import time_metrics
+
+if __name__ == '__main__':
+    fs = 250
+
+    # Load file
+    myFile = np.genfromtxt('dataset/141820_24hz.csv', delimiter=',')
+    data_matrix = np.delete(myFile, 0, 0)
+    green = data_matrix[:, 1]
+    unixtimestamps = data_matrix[:, 3]
+
+    # Interpolate ppg at fs
+    tAux = (unixtimestamps - unixtimestamps[0]) / 1000
+    t = np.arange(0, tAux[-1], 1 / fs)
+    cs = CubicSpline(tAux, -green)
+    ppg = cs(t)
+
+    # Baseline removal, filtering and normalization of PPG signal
+    ppg_filtered = filtering_and_normalization(ppg, fs)
+    ppg_filtered = remove_impulse_artifacts(ppg_filtered)
+
+    # Pulse detection
+    ppg_tk = ppg_pulse_detection(ppg_filtered, fs, plotflag=True)
+
+    # HRV
+    time_metrics(ppg_tk)
+    # ppg_tn = gap_corrector(ppg_tk, True) Developing...
